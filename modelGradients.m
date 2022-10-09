2080ff2faa46466d9ff8d167f817fa19c6f8ddd4
Seo-Dongmin <59951845+Seo-Dongmin@users.noreply.github.com>
2022-10-09 13:40:50 +0900

    Update modelGradients.m

--- modelGradients.m
+++ modelGradients.m
@@ -1,10 +1,10 @@
-function [gradients,loss] = modelGradients(MIMOME_Net,dlX,Y)
+function [gradients,loss] = modelGradients(NN_Net,dlX,Y)
 
-    dlYPred = forward(MIMOME_Net,dlX);
+    dlYPred = forward(NN_Net,dlX);
     dlYPred = sigmoid(dlYPred);
     
     loss = crossentropy(dlYPred,Y, 'TargetCategories','independent');
     
-    gradients = dlgradient(loss,MIMOME_Net.Learnables);
+    gradients = dlgradient(loss,NN_Net.Learnables);
 
 end
